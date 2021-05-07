b982a634ec5fb68c35d950e38a9b34444b02f3ee
Sina CHHUM <chhum.sina@ABABANK.COM>
2021-05-07 15:14:10 +0700

    asd

--- pages/index.vue
+++ pages/index.vue
@@ -18,133 +18,131 @@
       />
       <div>Sina testing</div>
     </div>
-    <pinch-zoom>
-      <div class="invoice-box scaled" style="background: #fff">
-        <div style="overflow: hidden">
-          <table class="table">
-            <tr class="titles">
-              <th>Navn</th>
-              <th>Antall</th>
-              <th>Enhet</th>
-              <th>Enhetspris</th>
-              <th>Rabatt</th>
-              <th>MVA</th>
-              <th>Sum eks MVA</th>
-            </tr>
-            <tr id="ProductList" class="item">
-              <td id="Product">
-                <span id="ProuductName">Medium Hosting<span></span></span>
-              </td>
-              <td>
-                <span id="ProductNumUnits">6<span></span></span>
-              </td>
-              <td>
-                <span id="ProductUnit">KG<span></span></span>
-              </td>
-              <td><span id="ProductUnitPrice">300kr</span></td>
-              <td><span id="ProductDiscount">Rabatt</span></td>
-              <td><span id="ProductTax">20%</span></td>
-              <td><span id="ProductCost">900 KR</span></td>
-            </tr>
-            <tr class="item">
-              <td>
-                <span id="ProuductName">Medium Hosting<span></span></span>
-              </td>
-              <td>
-                <span id="ProductNumUnits">6<span></span></span>
-              </td>
-              <td>
-                <span id="ProductUnit">KG<span></span></span>
-              </td>
-              <td><span id="ProductUnitPrice">300kr</span></td>
-              <td><span id="ProductDiscount">Rabatt</span></td>
-              <td><span id="ProductTax">20%</span></td>
-              <td><span id="ProductCost">900 KR</span></td>
-            </tr>
-            <tr class="item">
-              <td>
-                <span id="ProuductName">Medium Hosting<span></span></span>
-              </td>
-              <td>
-                <span id="ProductNumUnits">6<span></span></span>
-              </td>
-              <td>
-                <span id="ProductUnit">KG<span></span></span>
-              </td>
-              <td><span id="ProductUnitPrice">300kr</span></td>
-              <td><span id="ProductDiscount">Rabatt</span></td>
-              <td><span id="ProductTax">20</span>%</td>
-              <td>
-                <span id="ProductCost">900</span>
-                <span id="InvoiceCurrency">KR</span>
-              </td>
-            </tr>
-            <tr class="item">
-              <td>
-                <span id="ProuductName">Soup<span></span></span>
-              </td>
-              <td>
-                <span id="ProductNumUnits">6 <span></span></span>
-              </td>
-              <td>
-                <span id="ProductUnit">Liter <span></span></span>
-              </td>
-              <td><span id="ProductUnitPrice">300kr</span></td>
-              <td><span id="ProductDiscount">90%</span></td>
-              <td><span id="ProductTax">20%</span></td>
-              <td><span id="ProductCost">900 KR</span></td>
-            </tr>
-          </table>
+    <div class="invoice-box scaled" style="background: #fff">
+      <div style="overflow: hidden">
+        <table class="table">
+          <tr class="titles">
+            <th>Navn</th>
+            <th>Antall</th>
+            <th>Enhet</th>
+            <th>Enhetspris</th>
+            <th>Rabatt</th>
+            <th>MVA</th>
+            <th>Sum eks MVA</th>
+          </tr>
+          <tr id="ProductList" class="item">
+            <td id="Product">
+              <span id="ProuductName">Medium Hosting<span></span></span>
+            </td>
+            <td>
+              <span id="ProductNumUnits">6<span></span></span>
+            </td>
+            <td>
+              <span id="ProductUnit">KG<span></span></span>
+            </td>
+            <td><span id="ProductUnitPrice">300kr</span></td>
+            <td><span id="ProductDiscount">Rabatt</span></td>
+            <td><span id="ProductTax">20%</span></td>
+            <td><span id="ProductCost">900 KR</span></td>
+          </tr>
+          <tr class="item">
+            <td>
+              <span id="ProuductName">Medium Hosting<span></span></span>
+            </td>
+            <td>
+              <span id="ProductNumUnits">6<span></span></span>
+            </td>
+            <td>
+              <span id="ProductUnit">KG<span></span></span>
+            </td>
+            <td><span id="ProductUnitPrice">300kr</span></td>
+            <td><span id="ProductDiscount">Rabatt</span></td>
+            <td><span id="ProductTax">20%</span></td>
+            <td><span id="ProductCost">900 KR</span></td>
+          </tr>
+          <tr class="item">
+            <td>
+              <span id="ProuductName">Medium Hosting<span></span></span>
+            </td>
+            <td>
+              <span id="ProductNumUnits">6<span></span></span>
+            </td>
+            <td>
+              <span id="ProductUnit">KG<span></span></span>
+            </td>
+            <td><span id="ProductUnitPrice">300kr</span></td>
+            <td><span id="ProductDiscount">Rabatt</span></td>
+            <td><span id="ProductTax">20</span>%</td>
+            <td>
+              <span id="ProductCost">900</span>
+              <span id="InvoiceCurrency">KR</span>
+            </td>
+          </tr>
+          <tr class="item">
+            <td>
+              <span id="ProuductName">Soup<span></span></span>
+            </td>
+            <td>
+              <span id="ProductNumUnits">6 <span></span></span>
+            </td>
+            <td>
+              <span id="ProductUnit">Liter <span></span></span>
+            </td>
+            <td><span id="ProductUnitPrice">300kr</span></td>
+            <td><span id="ProductDiscount">90%</span></td>
+            <td><span id="ProductTax">20%</span></td>
+            <td><span id="ProductCost">900 KR</span></td>
+          </tr>
+        </table>
+      </div>
+      <div class="row">
+        <div class="equalHWrap eqWrap">
+          <div class="equalHW eq">
+            <table class="right">
+              <tr>
+                <td>
+                  <span style="display: inline-block; margin-right: 10px"
+                    ><strong>Total MVA:</strong></span
+                  >
+                </td>
+                <td>
+                  <span id="InvoceTotalVat">202</span>
+                  <span id="InvoiceCurrency1">KR</span><br />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span style="display: inline-block; margin-right: 10px"
+                    ><strong>Sum eks MVA:</strong></span
+                  >
+                </td>
+                <td>
+                  <span id="InvoiceSumExVat">900</span>
+                  <span id="InvoiceCurrency2">KR</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span style="display: inline-block; margin-right: 10px"
+                    ><strong>Å betale:</strong></span
+                  >
+                </td>
+                <td>
+                  <span id="ProductCost">1102</span>
+                  <span id="InvoiceCurrency3">KR</span>
+                </td>
+              </tr>
+            </table>
+          </div>
         </div>
-        <div class="row">
-          <div class="equalHWrap eqWrap">
-            <div class="equalHW eq">
-              <table class="right">
-                <tr>
-                  <td>
-                    <span style="display: inline-block; margin-right: 10px"
-                      ><strong>Total MVA:</strong></span
-                    >
-                  </td>
-                  <td>
-                    <span id="InvoceTotalVat">202</span>
-                    <span id="InvoiceCurrency1">KR</span><br />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span style="display: inline-block; margin-right: 10px"
-                      ><strong>Sum eks MVA:</strong></span
-                    >
-                  </td>
-                  <td>
-                    <span id="InvoiceSumExVat">900</span>
-                    <span id="InvoiceCurrency2">KR</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span style="display: inline-block; margin-right: 10px"
-                      ><strong>Å betale:</strong></span
-                    >
-                  </td>
-                  <td>
-                    <span id="ProductCost">1102</span>
-                    <span id="InvoiceCurrency3">KR</span>
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </div>
-          <div class="center">
-            <a href="https://sequra.no" style="text-decoration: none"
-              >Prøv gratis fakturerings program - rett i nettleseren
-              <span style="border-bottom: 1px solid #000">Sequra</span></a
-            >
-          </div>
+        <div class="center">
+          <a href="https://sequra.no" style="text-decoration: none"
+            >Prøv gratis fakturerings program - rett i nettleseren
+            <span style="border-bottom: 1px solid #000">Sequra</span></a
+          >
         </div>
       </div>
-    </pinch-zoom>
+    </div>
     <div
       style="
         position: fixed;
