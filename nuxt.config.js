d5db0cd8281320bdc4dde8c1b866e7191f65e88d
Sina CHHUM <chhum.sina@ABABANK.COM>
2021-05-07 14:29:25 +0700

    asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,11 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+      },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,86 @@
 <template>
   <div class="container">
-    <pinch-zoom>
+    <div
+      style="
+        position: fixed;
+        top: 0;
+        z-index: 11;
+        width: 100%;
+        background: #fff;
+        padding: 10px;
+      "
+    >
       <img
-        src="https://images.unsplash.com/photo-1544606402-e522c304b027?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1564&q=80"
+        src="https://www.ababank.com/typo3conf/ext/boxmodel/Resources/Private/Templates/ABA/images/aba-web-top-logo.png"
+        width="30"
+        height="30"
       />
+      <div>Sina testing</div>
+    </div>
+    <pinch-zoom>
+      <div style="background: #eee">
+        <img
+          style="width: 100%"
+          src="https://www.ababank.com/fileadmin/user_upload/PopUp_Windows/ABA_Instant_Account-Pop_up-EN.jpg"
+        />
+        <p>
+          No branch visit and paperwork — all you need is valid Khmer National
+          ID card and smartphone! Introducing ABA Instant Account – your first
+          bank account that you can open in the ABA Mobile app instantly from
+          anywhere and anytime. Open your Instant Account now to enjoy a safe,
+          modern, and convenient banking
+        </p>
+        <table>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+          <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+          </tr>
+          <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+          </tr>
+          <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+          </tr>
+          <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+          </tr>
+        </table>
+      </div>
     </pinch-zoom>
+    <div
+      style="
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        background: #ccc;
+        height: 56px;
+        padding-top: 15px;
+      "
+    >
+      <button>Pay now</button>
+    </div>
   </div>
 </template>
 
