019e436d9d92e33aea73d92fe59cbd5c4af0fd76
Sina CHHUM <chhum.sina@ABABANK.COM>
2021-05-07 15:55:23 +0700

    asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
       {
         name: 'viewport',
         content:
-          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+          'width=device-width, initial-scale=1.0, maximum-scale=0, user-scalable=no',
       },
       { hid: 'description', name: 'description', content: '' },
     ],
